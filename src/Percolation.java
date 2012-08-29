0b338b843e43ddc3a9c21f40505f57c2374f8003
Debajyoti Mahanta <debajyotimahanta@pcp001751pcs.aon.com>
2012-08-29 12:46:10 -0500

    Final version with all fix but performance

--- src/Percolation.java
+++ src/Percolation.java
@@ -2,136 +2,138 @@
  *Name: Debajyoti Mahanta
  * login: debajyoti.mahanta@gmail.com
  * date: Aug-27th 2012
- * This program is used to test weather a grid percolates or not using
- *  	- the grid size N of the percolation system.
- *    	- Creates an N-by-N grid of sites (initially all blocked)
- *    	- Reads in a sequence of sites (row i, column j) to open.
+ * This program is used to test the grid of size N
+ * It check if it percolates
  * ---------------------------------------------------------*/
 
 public class Percolation {
-	// used to store status of grid
-	private boolean[] id;
+    // used to store status of grid
+    private boolean[] id;
 
-	// initiate quickfind using virtual top and bottom
-	private WeightedQuickUnionUF objFind;
-	// Intimate quick find without virtual top and bottom to prevent bckwash
-	private WeightedQuickUnionUF objFind2;
-	// store local variable to capture grid size
-	private int Nmax;
+    // initiate quickfind using virtual top and bottom
+    private WeightedQuickUnionUF objFind;
+    // Intimate quick find without virtual top and bottom to prevent bckwash
+    private WeightedQuickUnionUF objFind2;
+    // store local variable to capture grid size
+    private int nMax;
 
-	private boolean isFirstJoinCalled = false;
+    private boolean isFirstJoinCalled = false;
 
-	/*---------------------------------------------------------
-	 *This is a constructor that takes in gride size and constructs an array of size N*N
-	 * ---------------------------------------------------------*/
+    /*---------------------------------------------------------
+     *This is a constructor that takes in gride size and constructs an array of size N*N
+     * ---------------------------------------------------------*/
 
-	public Percolation(int N) {
-		// added 2 for 2 virtual top
-		id = new boolean[N * N + 2];
+    public Percolation(int n) {
+        // added 2 for 2 virtual top
+        id = new boolean[n * n + 2];
 
-		for (int i = 0; i < N * N + 2; i++) {
+        for (int i = 0; i < n * n + 2; i++) {
 
-			id[i] = false;
+            id[i] = false;
 
-		}
-		// we are using +2 because we want to use virutal top and bottom
-		objFind = new WeightedQuickUnionUF(N * N + 2);
-		objFind2 = new WeightedQuickUnionUF(N * N);
+        }
+        // we are using +2 because we want to use virtual top and bottom
+        objFind = new WeightedQuickUnionUF(n * n + 2);
+        objFind2 = new WeightedQuickUnionUF(n * n);
 
-		Nmax = N;
-	}
+        nMax = n;
 
-	/*---------------------------------------------------------
-	 *open site (row i, column j) if it is not already
-	 *This will trow excepcption if the index are out of bound
-	 *will check the sourding cells to see if then are 
-	 * ---------------------------------------------------------*/
-	public void open(int i, int j) {
-		if (i <= 0 || i > Nmax)
-			throw new IndexOutOfBoundsException("row index i out of bounds");
-		if (j <= 0 || j > Nmax)
-			throw new IndexOutOfBoundsException("row index j out of bounds");
+        for (int i1 = 1; i1 <= nMax; i1++) {
+            objFind.union(0, i1);
+        }
+        for (int i1 = nMax * nMax; i1 > nMax * nMax - nMax; i1--) {
+            objFind.union(i1, nMax * nMax + 1);
+        }
+    }
 
-		// set value of indexs
-		id[Nmax * (i - 1) + j] = true;
-		boolean isJoinSuccess = false;
-		isJoinSuccess = shouldJoin(i - 1, j, i, j);
-		isJoinSuccess = shouldJoin(i + 1, j, i, j);
-		isJoinSuccess = shouldJoin(i, j - 1, i, j);
-		isJoinSuccess = shouldJoin(i, j + 1, i, j);
+    /*---------------------------------------------------------
+     *open site (row i, column j) if it is not already
+     *This will throw exception if the index are out of bound
+     *will check the sounding cells to see if then are 
+     * ---------------------------------------------------------*/
+    public void open(int i, int j) {
+        if (i <= 0 || i > nMax)
+            throw new IndexOutOfBoundsException("row index i out of bounds");
+        if (j <= 0 || j > nMax)
+            throw new IndexOutOfBoundsException("row index j out of bounds");
 
-		if (isJoinSuccess && !isFirstJoinCalled) {
-			// Initiate and connect virtual top and bottom
-			for (int i1 = 1; i1 <= Nmax; i1++) {
-				objFind.union(0, i1);
-			}
-			for (int i1 = Nmax * Nmax; i1 > Nmax * Nmax - Nmax; i1--) {
-				objFind.union(i1, Nmax * Nmax + 1);
-			}
-			isFirstJoinCalled = true;
-		}
-	}
+        // set value of index
+        id[nMax * (i - 1) + j] = true;
+        boolean isJoinSuccess = false;
+        isJoinSuccess = shouldJoin(i - 1, j, i, j);
+        isJoinSuccess = shouldJoin(i + 1, j, i, j);
+        isJoinSuccess = shouldJoin(i, j - 1, i, j);
+        isJoinSuccess = shouldJoin(i, j + 1, i, j);
 
-	/*---------------------------------------------------------
-	 * is site (row i, column j) open
-	 * returns the array where grid is open or closed is checked
-	 * ---------------------------------------------------------*/
-	public boolean isOpen(int i, int j) {
-		if (i <= 0 || i > Nmax)
-			throw new IndexOutOfBoundsException("row index i out of bounds");
-		if (j <= 0 || j > Nmax)
-			throw new IndexOutOfBoundsException("row index j out of bounds");
-		return id[Nmax * (i - 1) + j];
-	}
+        isFirstJoinCalled = true;
 
-	/*---------------------------------------------------------
-	 *is site (row i, column j) full
-	 *used the second find object to check if is full
-	 *it checks with all the elements in the top row
-	 * ---------------------------------------------------------*/
-	public boolean isFull(int i, int j) {
-		if (i <= 0 || i > Nmax)
-			throw new IndexOutOfBoundsException("row index i out of bounds");
-		if (j <= 0 || j > Nmax)
-			throw new IndexOutOfBoundsException("row index j out of bounds");
-		if (!isOpen(i, j))
-			return false;
-		// return objFind.connected(0,Nmax*(i-1)+j);
-		boolean isConnected = false;
-		for (int i1 = 0; i1 < Nmax; i1++) {
-			isConnected = objFind2.connected(i1, Nmax * (i - 1) + j - 1);
-			if (isConnected)
-				break;
-		}
-		return isConnected;
-	}
+    }
 
-	/*---------------------------------------------------------
-	 *does the system percolate?
-	 *it uses the first object, with virutal top and bottom to see if they are connected
-	 * ---------------------------------------------------------*/
-	public boolean percolates() {
-		return objFind.connected(0, Nmax * Nmax + 1);
-	}
+    /*---------------------------------------------------------
+     * is site (row i, column j) open
+     * returns the array where grid is open or closed is checked
+     * ---------------------------------------------------------*/
+    public boolean isOpen(int i, int j) {
+        if (i <= 0 || i > nMax)
+            throw new IndexOutOfBoundsException("row index i out of bounds");
+        if (j <= 0 || j > nMax)
+            throw new IndexOutOfBoundsException("row index j out of bounds");
 
-	/*---------------------------------------------------------
-	 *this is a private method that will first check if the two joins can be joined
-	 *if they can be joined, then the join from two object instance is called
-	 * ---------------------------------------------------------*/
-	private boolean shouldJoin(int l, int m, int x, int y) {
-		if (l <= 0 || l > Nmax)
-			return false;
-		if (m <= 0 || m > Nmax)
-			return false;
+        return id[nMax * (i - 1) + j];
 
-		int lm = Nmax * (l - 1) + m;
-		int xy = Nmax * (x - 1) + y;
+    }
 
-		if (isOpen(l, m)) {
-			objFind.union(lm, xy);
-			objFind2.union(lm - 1, xy - 1);
-		}
-		return true;
+    /*---------------------------------------------------------
+     *is site (row i, column j) full
+     *used the second find object to check if is full
+     *it checks with all the elements in the top row
+     * ---------------------------------------------------------*/
+    public boolean isFull(int i, int j) {
+        if (i <= 0 || i > nMax)
+            throw new IndexOutOfBoundsException("row index i out of bounds");
+        if (j <= 0 || j > nMax)
+            throw new IndexOutOfBoundsException("row index j out of bounds");
+        if (!isOpen(i, j))
+            return false;
+        // return objFind.connected(0,nMax*(i-1)+j);
+        boolean isConnected = false;
+        for (int i1 = 0; i1 < nMax; i1++) {
+            isConnected = objFind2.connected(i1, nMax * (i - 1) + j - 1);
+            if (isConnected)
+                break;
+        }
+        return isConnected;
+    }
 
-	}
+    /*---------------------------------------------------------
+     *does the system percolate?
+     *it uses the first object, with virtual top and bottom to see if they are connected
+     * ---------------------------------------------------------*/
+    public boolean percolates() {
+        if (isFirstJoinCalled)
+            return objFind.connected(0, nMax * nMax + 1);
+        else
+            return false;
+    }
+
+    /*---------------------------------------------------------
+     *this is a private method that will first check if the two joins can be joined
+     *if they can be joined, then the join from two object instance is called
+     * ---------------------------------------------------------*/
+    private boolean shouldJoin(int l, int m, int x, int y) {
+        if (l <= 0 || l > nMax)
+            return false;
+        if (m <= 0 || m > nMax)
+            return false;
+
+        int lm = nMax * (l - 1) + m;
+        int xy = nMax * (x - 1) + y;
+
+        if (isOpen(l, m)) {
+            objFind.union(lm, xy);
+            objFind2.union(lm - 1, xy - 1);
+        }
+        return true;
+
+    }
 }
--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -8,86 +8,91 @@
  * ---------------------------------------------------------*/
 
 public class PercolationStats {
-	private Percolation objPercolation;
-	// variable to hold number of times to computer
-	private double numberofCompute;
-	// number of times it took before it percolated
-	private double[] timestoPercolate;
+    private Percolation objPercolation;
+    // variable to hold number of times to computer
+    private double numberofCompute;
+    // number of times it took before it percolated
+    private double[] timestoPercolate;
 
-	// perform T independent computational experiments on an N-by-N grid
+    // perform T independent computational experiments on an N-by-N grid
 
-	/*---------------------------------------------------------
-	 * This is the constructed that takes
-	 * the grid size Gridsize and number of time to compute as Times
-	 * It throws exception if any of them is less then 0
-	 * It sets the private variable
-	 * it looks through top array and check if they are connected.
-	 * ---------------------------------------------------------*/
-	public PercolationStats(final int gridSize, final int times) {
-		if (gridSize <= 0)
-			throw new IllegalArgumentException("Gridsize cannot be zero");
-		if (times <= 0)
-			throw new IllegalArgumentException("Times cannot be zero");
-		int x;
-		int y;
-		numberofCompute = times;
-		timestoPercolate = new double[times];
-		for (int i = 0; i < numberofCompute; i++) {
-			objPercolation = new Percolation(gridSize);
-			int count = 0;
-			while (!objPercolation.percolates()) {
-				x = StdRandom.uniform(1, gridSize + 1);
-				y = StdRandom.uniform(1, gridSize + 1);
-				if (!objPercolation.isOpen(x, y)) {
-					objPercolation.open(x, y);
-					count++;
+    /*---------------------------------------------------------
+     * This is the constructed that takes
+     * the grid size Gridsize and number of time to compute as Times
+     * It throws exception if any of them is less then 0
+     * It sets the private variable
+     * it looks through top array and check if they are connected.
+     * ---------------------------------------------------------*/
+    public PercolationStats(final int gridSize, final int times) {
+        if (gridSize <= 0)
+            throw new IllegalArgumentException("Gridsize cannot be zero");
+        if (times <= 0)
+            throw new IllegalArgumentException("Times cannot be zero");
+        int x;
+        int y;
+        numberofCompute = times;
+        timestoPercolate = new double[times];
+        for (int i = 0; i < numberofCompute; i++) {
+            objPercolation = new Percolation(gridSize);
+            int count = 0;
+            while (!objPercolation.percolates()) {
+                x = StdRandom.uniform(1, gridSize + 1);
+                y = StdRandom.uniform(1, gridSize + 1);
+                if (!objPercolation.isOpen(x, y)) {
+                    objPercolation.open(x, y);
+                    count++;
 
-				}
+                }
 
-			}
-			double d = (double) count / (double) (gridSize * gridSize);
-			timestoPercolate[i] = d;
-		}
+            }
+            double d = (double) count / (double) (gridSize * gridSize);
+            timestoPercolate[i] = d;
+        }
 
-	}
+    }
 
-	/*---------------------------------------------------------
-	 * Get the mean of all the percolation threshold
-	 * ---------------------------------------------------------*/
-	public double mean() {
-		return StdStats.mean(timestoPercolate);
-	}
+    /*---------------------------------------------------------
+     * Get the mean of all the percolation threshold
+     * ---------------------------------------------------------*/
+    public double mean() {
+        return StdStats.mean(timestoPercolate);
+    }
 
-	/*---------------------------------------------------------
-	 *  sample standard deviation of percolation threshold
-	 * ---------------------------------------------------------*/
-	public double stddev() {
-		return StdStats.stddev(timestoPercolate);
-	}
+    /*---------------------------------------------------------
+     *  sample standard deviation of percolation threshold
+     * ---------------------------------------------------------*/
+    public double stddev() {
+        return StdStats.stddev(timestoPercolate);
+    }
 
-	/*---------------------------------------------------------
-	 *  the main method used for testing via command line
-	 * ---------------------------------------------------------*/
-	public static void main(String[] args) // test client, described below
-	{
+    /*---------------------------------------------------------
+     *  the main method used for testing via command line
+     * ---------------------------------------------------------*/
+    public static void main(String[] args) // test client, described below
+    {
 
-		int size = Integer.parseInt(args[0]);
-		int times = Integer.parseInt(args[1]);
-		Stopwatch s = new Stopwatch();
-		PercolationStats x = new PercolationStats(size, times);
-		double time = s.elapsedTime();
+        int size = Integer.parseInt(args[0]);
+        int times = Integer.parseInt(args[1]);
+        Stopwatch s = new Stopwatch();
+        PercolationStats x = new PercolationStats(size, times);
+        double time = s.elapsedTime();
 
-		double mu = x.mean();
-		double sigma = x.stddev();
-		double confminus = mu - ((1.96 * sigma) / Math.sqrt(size));
-		double confplus = mu + ((1.96 * sigma) / Math.sqrt(times));
+        double mu = x.mean();
+        double sigma = x.stddev();
+        double confminus = mu - ((1.96 * sigma) / Math.sqrt(size));
+        double confplus = mu + ((1.96 * sigma) / Math.sqrt(times));
 
-		StdOut.println(time);
-		StdOut.println(mu);
-		StdOut.println(sigma);
-		StdOut.print(Double.toString(confminus));
-		StdOut.print(", ");
-		StdOut.println(Double.toString(confplus));
+        StdOut.println(time);
+        StdOut.println(mu);
+        StdOut.println(sigma);
+        StdOut.print(Double.toString(confminus));
+        StdOut.print(", ");
+        StdOut.println(Double.toString(confplus));
 
-	}
+        // Percolation x = new Percolation(1);
+        // boolean y = x.percolates();
+        // x.open(1, 1);
+        // y = x.percolates();
+
+    }
 }
